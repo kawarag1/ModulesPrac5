9c71931290a6d3c45efaa721bf02ebc57312bf97
kawarag1 <dima.kretinin.05@mail.ru>
2025-12-18 22:37:51 +0700

    added more funcs

--- ModulesPrac5/Pages/CartPage.xaml.cs
+++ ModulesPrac5/Pages/CartPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ModulesPrac5.Models;
+using ModulesPrac5.Services;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,30 @@ namespace ModulesPrac5.Pages
     /// </summary>
     public partial class CartPage : Page
     {
-        public CartPage()
+        public static Users user_;
+        public CartPage(Users user)
         {
             InitializeComponent();
+            user_ = user;
+            InitializeCarts();
+        }
+
+        private void InitializeCarts()
+        {
+            var context = Helper.GetContext();
+            var carts = context.Carts.Where(x => x.UserID == user_.ID).Include("Products").ToList();
+            LVCarts.ItemsSource = carts;
+            CartsSumProdcuts.Text = "Количество уникальных товаров: " + carts.Count.ToString();
+            decimal sum = 0;
+            foreach (var cart in carts)
+            {
+                sum += cart.Products.Price;
+            }
+            CartCost.Text = sum.ToString() + "₽";
+        }
+        private void CreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- ModulesPrac5/Pages/ShopPage.xaml.cs
+++ ModulesPrac5/Pages/ShopPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ModulesPrac5.Models;
+using ModulesPrac5.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,8 @@ namespace ModulesPrac5.Pages
         public ShopPage()
         {
             InitializeComponent();
+            InitializeProducts();
+            InitializeCategories();
         }
 
         private void CartBtn_Click(object sender, RoutedEventArgs e)
@@ -30,19 +34,84 @@ namespace ModulesPrac5.Pages
             NavigationService.Navigate(new CartPage());
         }
 
+        private void InitializeProducts()
+        {
+            var context = Helper.GetContext();
+            var products = context.Products;
+            LViewItems.ItemsSource = products;
+        }
+
+        private void InitializeCategories()
+        {
+            var context = Helper.GetContext();
+            OrderByCategories.ItemsSource = context.CategoriesProducts.ToList();
+        }
+
         private void OrderByName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            var context = Helper.GetContext();
+            if (OrderByName.SelectedItem.ToString() == "А - Я")
+            {
+                LViewItems.ItemsSource = null;
+                LViewItems.ItemsSource = context.Products.OrderBy(x => x.Name);
+            }
+            else if (OrderByName.SelectedItem.ToString() == "Я - А")
+            {
+                LViewItems.ItemsSource = null;
+                LViewItems.ItemsSource = context.Products.OrderByDescending(x => x.Name);
+            }
         }
 
         private void OrderByCost_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            var context = Helper.GetContext();
+            if (OrderByName.SelectedItem.ToString() == "По возрастанию цены")
+            {
+                LViewItems.ItemsSource = null;
+                LViewItems.ItemsSource = context.Products.OrderBy(x => x.Price);
+            }
+            else if (OrderByName.SelectedItem.ToString() == "По убыванию цены")
+            {
+                LViewItems.ItemsSource = null;
+                LViewItems.ItemsSource = context.Products.OrderByDescending(x => x.Price);
+            }
         }
 
         private void OrderByCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            var context = Helper.GetContext();
+            LViewItems.ItemsSource = null;
+            LViewItems.ItemsSource = context.Products.Where(x => x.CategoryID == OrderByCategories.SelectedIndex + 1).ToList();
+        }
 
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserHelper.user == null)
+            {
+                MessageBox.Show("Вы не авторизованы!");
+            }
+            else
+            {
+                var button = sender as Button;
+                var item = button.DataContext as Products;
+                var context = Helper.GetContext();
+
+                var product_ = context.Carts.Where(x => x.UserID == UserHelper.user.ID && x.ProductID == item.ID).FirstOrDefault();
+                if (product_ != null)
+                {
+                    product_.Quantity += 1;
+                    context.SaveChanges();
+                }
+                else
+                {
+                    var cart = new Carts();
+                    cart.Quantity = 1;
+                    cart.ProductID = item.ID;
+                    cart.UserID = UserHelper.user.ID;
+                    context.Carts.Add(cart);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
